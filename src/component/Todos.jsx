54edc810f9a5bf6d4a1bd6fd404e8e1d0ac66a5b
Ayush Rusiya <ayushrusiya9@gmail.com>
2025-11-01 11:10:36 +0530

    update

--- src/component/Todos.jsx
+++ src/component/Todos.jsx
@@ -3,6 +3,7 @@ import axios from 'axios'
 
 const Todos = () => {
     const [data, setData] = useState([])
+    const [task, setTask] = useState('')
 
     const BASE_URL = 'http://localhost:3000/todos'
 
@@ -12,24 +13,73 @@ const Todos = () => {
         console.log(res.data)
     }
 
-    useEffect(()=>{
-        // getTodos();
-        axios.get(BASE_URL)
-        .then((res)=> {setData(res.data);
-            console.log(res.data)})
-    
-    },[])
+    const addTask = () => {
+        axios.post(BASE_URL, { task, completed: false })
+            .then(() => {
+                setTask('');
+                getTodos();
+            }
+            )
+    }
 
+    const toggleComplete = (id, currentStatus) =>{
+        axios.patch(`${BASE_URL}/${id}`,{
+            completed: !currentStatus
+        })
+        .then(() => getTodos())
+    }
+
+    useEffect(() => {
+        getTodos();
+        // axios.get(BASE_URL)
+        //     .then((res) => {
+        //         setData(res.data);
+        //         console.log(res.data)
+        //     })
+    }, [])
+
+    const liStyle = {
+        display: "flex",
+        alignItems: "center",
+        padding: "8px 12px",
+        gap: "10px",
+    }
+
+
+    let btn = {
+        width: "55px",
+        height: "25px",
+    }
 
     return (
-        <div>
-            {
-                data.map((res) => {
-                <ul>
-                        <li>{res}</li>
-                </ul>
-                    })
-            }
+        <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+            <h1>todos</h1>
+            <div>
+
+                <input type="text" name="task" placeholder='Enter Your Task.....'
+                    value={task}
+                    onChange={(e) => setTask(e.target.value)}
+                />
+                <button onClick={addTask}>Add</button>
+
+            </div>
+            <ul>
+                {
+                    data.map(e => (
+                        <li style={liStyle} key={e.id}>
+                            <input type="checkbox" name="completed"
+                            checked={e.completed}
+                            onChange={() => toggleComplete(e.id, e.completed)}
+                            />
+                            <h4 
+                                style={{textDecoration: e.completed ? "line-through" : "none"}}
+                            >{e.task}</h4>
+                            <button style={btn}>Edit</button>
+                            <button style={btn}>Delete</button>
+                        </li>
+                    ))
+                }
+            </ul>
         </div>
     )
 }
